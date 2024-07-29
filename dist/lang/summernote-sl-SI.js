324f6407313d8e84c7ea13e9b6c14a7073601a6b
Sijun Kang <kangsijun@gmail.com>
2024-07-29 18:14:44 -0400

    build with pasteHTML fix

--- dist/lang/summernote-sl-SI.js
+++ dist/lang/summernote-sl-SI.js
@@ -1,8 +1,27 @@
-/******/ (() => { // webpackBootstrap
+/*!
+ * 
+ * Super simple WYSIWYG editor v0.8.19
+ * https://summernote.org
+ *
+ *
+ * Copyright 2013- Alan Hong and contributors
+ * Summernote may be freely distributed under the MIT license.
+ *
+ * Date: 2024-07-29T22:14Z
+ *
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(self, () => {
+return /******/ (() => { // webpackBootstrap
 var __webpack_exports__ = {};
-/*!**************************************!*\
-  !*** ./src/lang/summernote-sl-SI.js ***!
-  \**************************************/
 (function ($) {
   $.extend(true, $.summernote.lang, {
     'sl-SI': {
@@ -158,6 +177,8 @@ var __webpack_exports__ = {};
     }
   });
 })(jQuery);
+/******/ 	return __webpack_exports__;
 /******/ })()
 ;
+});
 //# sourceMappingURL=summernote-sl-SI.js.map
